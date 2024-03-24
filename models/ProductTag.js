802c22add465558c16a45ef61ce69a4a391f8d29
t4k1 <johncurtis702@gmail.com>
2024-03-23 18:11:19 -0700

    added models and routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize')
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection')
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -15,6 +34,6 @@ ProductTag.init(
     underscored: true,
     modelName: 'product_tag',
   }
-);
+)
 
-module.exports = ProductTag;
+module.exports = ProductTag
